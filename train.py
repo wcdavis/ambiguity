6b12067d076f7b8f5a5fac3c4ea5f60fa008daf6
Ubuntu <ubuntu@ip-172-31-3-126.ec2.internal>
2016-04-29 11:17:44 +0000

    better printing

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from text_cnn import TextCNN
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
+tf.flags.DEFINE_string("filter_sizes", "11,12,13", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularizaion lambda (default: 0.0)")
@@ -190,7 +190,7 @@ with tf.Graph().as_default():
 #	    print y_output
 	    print "Prediction: ", pred_0, pred_1
             print "Actual:     ", actual_0, actual_1
-	    print classification_report(labels, actual_output)
+	    print classification_report(labels, actual_output, digits=4)
 	    print confusion_matrix(labels, actual_output)
             if writer:
                 writer.add_summary(summaries, step)
